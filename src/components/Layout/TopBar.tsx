e1f9427d3cb0535b14d5027a54d6b2e8d1e52689
Brendan Forster <bpforster24+products@gmail.com>
2024-07-19 16:58:56 -0700

    refactored TopBar to be responsive

--- src/components/Layout/TopBar.tsx
+++ src/components/Layout/TopBar.tsx
@@ -2,11 +2,18 @@ import { Link } from "react-router-dom";
 
 export default function TopBar() {
     return (
-        <header className="h-16 w-full min-w-[720px] max-w-[1200px] mt-16 px-20 py-4">
+        <header className="h-16 w-full max-w-[1200px] mx-auto">
             <nav className="flex justify-between items-center">
-                <Link to="#"> Site Icon </Link>
-                <Link to="#"> Site Name </Link>
-                <a href="#">Connect Wallet</a>
+                <Link to="#">Site Icon</Link>
+                <Link to="#" className="hidden sm:block">
+                    Site Name
+                </Link>
+                <a
+                    href="#"
+                    className="hidden sm:block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
+                >
+                    Connect Wallet
+                </a>
             </nav>
         </header>
     );
