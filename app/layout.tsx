071be6805180c1e1710402ab9a62b3510bf33260
Brendan Forster <bpforster24+products@gmail.com>
2024-10-21 20:26:41 -0700

    added providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,36 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { AppProviders } from "@/app/AppProviders"
 
 const geistSans = localFont({
-  src: "../src/fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+    src: "../src/fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
 });
 const geistMono = localFont({
-  src: "../src/fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+    src: "../src/fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: "Eternal Vigilance",
-  description: "Trying to make sure the future is awesome",
+    title: "Eternal Vigilance",
+    description: "Trying to make sure the future is awesome",
 };
 
 export default async function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                <AppProviders>{children}</AppProviders>
+            </body>
+        </html>
+    );
 }
