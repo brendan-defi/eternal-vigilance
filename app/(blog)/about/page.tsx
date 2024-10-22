c1c2334bf6604ccf0a8766fc6b45167d73742959
Brendan Forster <bpforster24+products@gmail.com>
2024-10-21 21:19:52 -0700

    Added a salutation to the About page

--- app/(blog)/about/page.tsx
+++ app/(blog)/about/page.tsx
@@ -2,11 +2,19 @@
 
 import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
-
+import { useAccount } from "wagmi";
+import { useName } from "@coinbase/onchainkit/identity";
 import fetchPostText from "@/src/utils/fetchPostText";
+import { isDevelopment } from "@/app/constants";
+import { base, baseSepolia } from "viem/chains";
 
 export default function AboutPage() {
     const [about, setAbout] = useState("");
+    const { address } = useAccount();
+    const { name } = useName({
+        address: address ?? "0x",
+        chain: isDevelopment ? baseSepolia : base,
+    });
 
     useEffect(() => {
         const fetchAbout = async () => {
@@ -18,6 +26,10 @@ export default function AboutPage() {
 
     return (
         <div className="about">
+            <h1>
+                Hello{" "}
+                {name ?? `${address?.slice(0, 6)}...${address?.slice(-4)}`}
+            </h1>
             <ReactMarkdown>{about}</ReactMarkdown>
         </div>
     );
