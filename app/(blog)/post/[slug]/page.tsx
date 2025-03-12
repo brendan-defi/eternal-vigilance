b828e3a8da7143c3e6a293420927b16c627fae45
Brendan Forster <bpforster24+products@gmail.com>
2025-03-11 23:24:10 -0700

    fix params

--- app/(blog)/post/[slug]/page.tsx
+++ app/(blog)/post/[slug]/page.tsx
@@ -1,20 +1,18 @@
 import ReactMarkdown from "react-markdown";
 
 import { postsIndex } from "@/src/assets/posts/postsIndex";
+import { type PostRef } from "@/src/types/post";
 import fetchPostText from "@/src/utils/fetchPostText";
 import parsePost from "@/src/utils/parsePost";
-import { type PostRef } from "@/src/types/post";
 
-type BlogPostParams = {
-    params: PostRef;
-};
+type BlogPostParams = Promise<PostRef>;
 
 export async function generateStaticParams() {
     return postsIndex;
 }
 
-export default async function BlogPost({ params }: BlogPostParams) {
-    const slug = params.slug;
+export default async function BlogPost({ params }: { params: BlogPostParams }) {
+    const { slug } = await params;
 
     const rawPost = await fetchPostText(slug);
     const post = parsePost({ slug }, rawPost);
