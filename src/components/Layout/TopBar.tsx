2dd6477be3ce5180a88698bed2b39fabb06417bd
Brendan Forster <bpforster24+products@gmail.com>
2025-03-12 22:58:08 -0700

    minor updates

--- src/components/Layout/TopBar.tsx
+++ src/components/Layout/TopBar.tsx
@@ -7,10 +7,11 @@ export default function TopBar() {
     return (
         <header className="h-16 w-full max-w-[1200px] mx-auto px-4 sm:px-4">
             <nav className="flex justify-between items-center">
-                <Link href="/" className="hidden sm:block">
+                <Link href="/" className="max-sm:block">
                     <Image
                         src={EVLogo as StaticImageData}
                         alt="eternal vigilance logo"
+                        priority
                     />
                 </Link>
                 <WalletAdvancedDefault />
