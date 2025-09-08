3de3c3f3499f1b4b4754252ddc939356d5a74ab9
Brendan Forster <bpforster24+products@gmail.com>
2025-09-08 14:24:48 -0400

    post

--- src/assets/posts/_postsIndex.ts
+++ src/assets/posts/_postsIndex.ts
@@ -2,6 +2,9 @@ import { PostRef } from "@/src/types/post";
 
 export const postsIndex: PostRef[] = [
     {
+        slug: "the_next_right_thing.md",
+    },
+    {
         slug: "hello_neo.md",
     },
 ];
