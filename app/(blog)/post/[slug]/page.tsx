7a1510057b32c0563b015c24b8520f6dc2f85229
Brendan Forster <bpforster24+products@gmail.com>
2025-09-08 14:19:02 -0400

    update import, add date

--- app/(blog)/post/[slug]/page.tsx
+++ app/(blog)/post/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import ReactMarkdown from "react-markdown";
 
-import { postsIndex } from "@/src/assets/posts/postsIndex";
+import { postsIndex } from "@/src/assets/posts/_postsIndex";
 import { type PostRef } from "@/src/types/post";
 import fetchPostText from "@/src/utils/fetchPostText";
 import parsePost from "@/src/utils/parsePost";
@@ -20,6 +20,9 @@ export default async function BlogPost({ params }: { params: BlogPostParams }) {
     return (
         <div id="PostContainer" className="mx-4">
             <h1>{post?.postData?.title}</h1>
+            <span className="text-sm text-gray-500 italic">
+                {post?.postData?.publishedAt}
+            </span>
             <ReactMarkdown>{post?.postText ?? ""}</ReactMarkdown>
         </div>
     );
